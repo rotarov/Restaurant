b67ea9e6f737877299c01f6ef676b137363fc6fa
Rasul Otarov <ibn-mussa@yandex.ru>
2017-04-29 20:17:22 +0300

    add changes..

---
+++ src/main/java/io/khasang/restaurant/config/AppConfig.java
@@ -0,0 +1,7 @@
+package io.khasang.restaurant.config;
+
+/**
+ * Created by user on 29.04.2017.
+ */
+public class AppConfig {
+}
---
+++ src/main/java/io/khasang/restaurant/model/Massage.java
@@ -0,0 +1,7 @@
+package io.khasang.restaurant.model;
+
+/**
+ * Created by user on 29.04.2017.
+ */
+public class Massage {
+}
